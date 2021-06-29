d919a8424fd7c46839c4f3074dbaa3aad3d423a3
Victor Portela <portelavictor@hotmail.com>
2021-06-29 11:38:49 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/vportela/workshopmongo/resource/UserResource.java
+++ src/main/java/com/vportela/workshopmongo/resource/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vportela.workshopmongo.domain.Post;
 import com.vportela.workshopmongo.domain.User;
 import com.vportela.workshopmongo.dto.UserDTO;
 import com.vportela.workshopmongo.services.UserService;
@@ -62,6 +63,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
